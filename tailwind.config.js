776ec0faab6833f210b4abfeb844cc74da685483
vintech05 <kelanjutannyakelvin@gmail.com>
2024-02-19 22:23:37 +0700

    added custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}"
+  ],
   theme: {
     extend: {
-      colors: {
-        'dark-blue': 'hsl(233, 26%, 24%)',
-        'lime-green': 'hsl(136, 65%, 51%)',
-        'bright-cyan': 'hsl(192, 70%, 51%)',
-
-        'Grayish-Blue': 'hsl(233, 8%, 62%)',
-        'Light-Grayish Blue': 'hsl(220, 16%, 96%)',
-        'Very-Light-Gray': 'hsl(0, 0%, 98%)',
-        'White': 'hsl(0, 0%, 100%)',
-
+      fontFamily: {
+        'Public': ['Public Sans, sans-serif'],
       },
     },
   },
